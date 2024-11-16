82df171c5f81e335a815768872754ffe14b2fef4
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-11-16 11:24:16 +0100

    First Implementation of  wrapper class for lib

--- src/main/java/SafeStorage/util/EncryptionUtil.java
+++ src/main/java/SafeStorage/util/EncryptionUtil.java
@@ -1,5 +1,21 @@
 package SafeStorage.util;
 
+//TODO: Fix package naming in the lib, is stringCryption, should be StringCryption.
+import main.java.stringCryption.StringCryption;
+
 public class EncryptionUtil {
-    
+
+  private final StringCryption stringCryption;
+
+  public EncryptionUtil() {
+    this.stringCryption = new StringCryption();
+  }
+
+  public String encrypt(String content, int encryptionLevel) {
+    return stringCryption.encryptAtChosenLevel(content, encryptionLevel);
+  }
+
+  public String decrypt(String encryptedContent, int encryptionLevel) {
+    return stringCryption.decryptAtChosenLevel(encryptedContent, encryptionLevel);
+  }
 }
